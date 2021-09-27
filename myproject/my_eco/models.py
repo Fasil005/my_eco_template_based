6f8b6efc9c40b0304d6c3c3df76c15abf677ecaf
Fasil005 <fasilkeedath@gmail.com>
2021-09-27 21:40:15 +0530

    ......

--- myproject/my_eco/models.py
+++ myproject/my_eco/models.py
@@ -2,6 +2,10 @@ from django.db import models as md
 
 # Create your models here.
 
+class User(md.Model):
+    id = md.AutoField(primary_key=True)
+    email = md.EmailField(max_length=255)
+    password = md.CharField(max_length=150)
 class Statements(md.Model):
 
     id = md.AutoField(primary_key=True)
