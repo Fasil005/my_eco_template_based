9d721ac290a37a7d6cc2a41965df41dd87bb3a05
Fasil005 <fasilkeedath@gmail.com>
2021-09-27 22:04:44 +0530

    ....

--- myproject/my_eco/views.py
+++ myproject/my_eco/views.py
@@ -1,16 +1,16 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render,redirect
 
-from rest_framework.viewsets import ModelViewSet
+
 from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny
 from .models import Statements
 from .serializers import StatementsSerializer
-from django.http import HttpResponse
 
 
 def index(request):
-    statements = Statements.objects.all()
-    return render(request, 'index.html', {'statements': statements})
+    statements = Statements.objects.order_by('-id').all()
+    main_balance = list(statements)[0].balance
+    return render(request, 'index.html', {'statements': statements,'main_balance':main_balance})
 
 def expense(request):
     return render(request, 'expense.html', {})
@@ -21,8 +21,15 @@ class ADDExpense(APIView):
         balance = Statements.objects.order_by('-id').first().balance
         data = request.data.copy()
         data['balance'] = float(balance) - float(data['amount'])
+        if data['balance'] < 0:
+            return render(request, 'expense.html', {'msg':'Insufficient Funds'})
         serializer = StatementsSerializer(data=data)
         if serializer.is_valid():
             serializer.save()
             data = serializer.data
-            return render(request, 'expense.html', {})
+            return redirect('/')
+        error=""
+        for i in serializer.errors:
+            error = serializer.errors.get(i)[0].replace('This',i)
+            break
+        return render(request, 'expense.html', {'msg': error})
