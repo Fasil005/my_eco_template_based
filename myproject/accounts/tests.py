94e7b10398825c269f76cdd64e4ecde4596451aa
FasilSparky005 <muhammad@sparksupport.com>
2022-08-03 13:00:00 +0530

    Untrack files in .gitignore

--- myproject/accounts/tests.py
+++ myproject/accounts/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
-# Create your tests here.
+from accounts.models import User
+
+
+class UserTestCase(TestCase):
+    def setUp(self):
+        User.objects.create(fullname="lion", username="roar")
+        User.objects.create(fullname="cat", username="meow")
+    
+    def test_user(self):
+        user = User.objects.all()
+        print(user)
