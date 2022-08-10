29546e7779245e605ce101e94a4b4cfce9a17f02
fasil005 <fasilkeedath@gmail.com>
2022-08-10 10:59:31 +0530

    Dockerized with postgres

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'PORT': '5432',
+        'HOST': 'db',
     }
 }
 
