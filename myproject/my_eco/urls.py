f692594ed2b4cedb46a17c5ac74cf236927da2ad
fasil005 <fasilkeedath@gmail.com>
2022-08-11 12:26:02 +0530

    Balance update functionality complete

--- myproject/my_eco/urls.py
+++ myproject/my_eco/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('add_expense', views.ADDExpense.as_view(),name='add_expense'),
     path('add_income', views.ADDIncome.as_view(),name='add_inome'),
     path('add_loan', views.ADDLoan.as_view(),name='add_loan'),
+    path('add_balance', views.ADDBalance.as_view(),name='add_balance'),
     path('income', views.income,name='income'),
     url('expense', views.expense,name='expense'),
     url('loan', views.loan,name='loan'),
